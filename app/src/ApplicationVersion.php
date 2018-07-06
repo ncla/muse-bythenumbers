253dde6a09eb46789f7c77dc7cac555a3eb825c5
ncla <ncla@users.noreply.github.com>
2018-07-06 15:58:28 +0300

    Fixed incorrect datetime formatting for application version

--- app/src/ApplicationVersion.php
+++ app/src/ApplicationVersion.php
@@ -9,17 +9,11 @@ namespace App\Src;
 
 class ApplicationVersion
 {
-    const MAJOR = 1;
-    const MINOR = 2;
-    const PATCH = 3;
-
     public static function get()
     {
         $commitHash = trim(exec('git log --pretty="%h" -n1 HEAD'));
+        $commitDate = new \DateTime(trim(exec('git log --pretty="%ci" -n1 HEAD')));
 
-        $commitDate = new \DateTime(trim(exec('git log -n1 --pretty=%ci HEAD')));
-        $commitDate->setTimezone(new \DateTimeZone('UTC'));
-
-        return sprintf('%s (%s)', $commitHash, $commitDate->format('Y-m-d H:m:s'));
+        return sprintf('%s (%s)', $commitHash, $commitDate->format('Y-m-d H:i:s'));
     }
 }
